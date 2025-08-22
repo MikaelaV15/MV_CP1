c2759c322c0b0b6daf4d6df8cd24b7263a9aab79
mikaelav15 <mikaela.vela@ucas-edu.net>
2025-08-22 09:52:41 -0600

    who_are_you.py

---
+++ Practices/who_are_you.py
@@ -0,0 +1,7 @@
+# MV 1st Who are you
+
+name = input("What is your name: ")
+age = input("What is your age: ")
+favorite_color = input("What is your favorite color: ")
+
+print("Wow" ,name, "I like your name, Wow" ,age, "I am the same age too!" ,favorite_color, "is my favorite color too!")
