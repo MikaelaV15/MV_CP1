eace6d255bd469a43d8ed3877b424940a51fbc88
mikaelav15 <mikaela.vela@ucas-edu.net>
2025-08-25 09:20:25 -0600

    Created projects folder

---
+++ Projects/who_are_you.py
@@ -0,0 +1,7 @@
+# MV Who are you
+
+name = input("What is your name: ")
+age = input("What is your age: ")
+favorite_color = input("What is your favorite color: ")
+
+print("Hello" ,name, "You look very young for" ,age, "You picked a good color! The color",favorite_color, "is my favorite to!")
---
+++ notes/starters/august_25.py
@@ -0,0 +1,4 @@
+number = 5
+number = 10
+number = 0 
+number = 42
