8a1e136543f3aa7cd5ef2e18f3eacff11e4a2fcf
MikaelaV15 <mikaela.vela@ucas-edu>
2025-08-29 09:53:13 -0600

    fixed

--- Practices/average_grade.py
+++ Practices/average_grade.py
@@ -1,13 +1,15 @@
 # MV 1st Average Grade
 
 cs_grade = int(input("What is your grade in cs: "))
-math_grade = input("What is your grade in math: ")
-seminary_grade = input("What is your grade in seminary: ")
-advisory_grade = input("What is your grade in advisory: ")
-english_grade = input("What is your grade in english: ")
-art_grade = input("What is your grade in art: ")
-biology_grade = input("What is your grade in biology: ")
-average_grade = 0
+math_grade = int(input("What is your grade in math: "))
+seminary_grade = int(input("What is your grade in seminary: "))
+advisory_grade = int(input("What is your grade in advisory: "))
+english_grade = int(input("What is your grade in english: "))
+art_grade = int(input("What is your grade in art: "))
+biology_grade = int(input("What is your grade in biology: "))
+                          
 average = (cs_grade + math_grade + seminary_grade + advisory_grade + english_grade + art_grade + biology_grade)
 average_grade = average/7
 print(average_grade)
+
+round(average_grade ,2)
