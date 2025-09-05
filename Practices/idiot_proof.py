c7d06fffd46fe44ff6d32c5e73b53adf2630cf4d
MikaelaV15 <mikaela.vela@ucas-edu>
2025-09-05 09:53:17 -0600

    idiot proof

---
+++ Practices/idiot_proof.py
@@ -0,0 +1,6 @@
+# MV 1st Idiot Proof
+
+full_name = input("What is your full name?: ") 
+full_name = input().strip().title()
+phone_number = input("What is your phone number?: ")
+gpa = input("What is your gpa?: ")
