e203593694dd49d7a9588f1188a495c016149a10
MikaelaV15 <mikaela.vela@ucas-edu>
2025-09-10 09:49:33 -0600

    fixed folder

--- Practices/calculator.py
+++ Practices/calculator.py
@@ -1,2 +1,13 @@
-# MV 1st Basic Calculator
+# MV 1st Basic calculator 
 
+number_1 = int(input("What is number 1: "))
+number_2 = int(input("What is number 2: "))
+
+print(number_1+number_2) 
+print(number_1-number_2) 
+print(number_1*number_2) 
+print(number_1/number_2)  
+print(number_1//number_2)  
+print(number_1%number_2) 
+
+print(round(0.83333333333333334)) # 0.83
