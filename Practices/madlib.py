96def400623f1d6dbad385d5ad128cfe7b4ac98f
MikaelaV15 <mikaela.vela@ucas-edu>
2025-09-16 09:53:15 -0600

    madlib created

--- Practices/madlib.py
+++ Practices/madlib.py
@@ -1,1 +1,31 @@
 #MV 1st Madlib
+
+print("Give me a noun")
+noun1 = input()
+
+print("Give me a plural noun")
+plural_noun1 = input()
+
+print("Give me a verb ending in ing")
+verb_ing1 = input()
+
+print("Give me a plural noun")
+plural_noun2 = input()
+
+print("Give me a city")
+city = input()
+
+print("Give me a plural noun")
+plural_noun3 = input()
+
+print("Give me an adjective")
+adjective1 = input ()
+
+print("Give me a noun")
+noun2 = input()
+
+print("Give me a number")
+number1 = input()
+
+print("Give me a noun")
+noun3 = input()
