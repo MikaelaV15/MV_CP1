6086e4f158ae97d6952399d8c54cbd97c1f9f96d
MikaelaV15 <mikaela.vela@ucas-edu.net>
2025-09-19 09:33:47 -0600

    project finished

--- Practices/idiot_proof.py
+++ Practices/idiot_proof.py
@@ -1,11 +1,9 @@
 # MV 1st Idiot Proof
 
-full_name = input("What is your full name?: ") 
-full_name = input().strip().title()
+full_name = input("What is your full name?: ")
 phone_number = input("What is your phone number?: ")
-phone_number = input().strip()
 gpa = input("What is your gpa?: ")
-gpa = input().strip()
 
-print = ("Hello" , full_name, "that is a good number")
+
+print("Hello" + full_name + "Can I text your number" + phone_number + "sometime? You have a very high gpa!" + gpa) 
 
