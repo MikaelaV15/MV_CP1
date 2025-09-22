1090caef2c99bd17bede49e709f5220579abf2f3
MikaelaV15 <mikaela.vela@ucas-edu.net>
2025-09-22 09:53:18 -0600

    folder created and code fixed

---
+++ Practices/user_signin.py
@@ -0,0 +1,13 @@
+# MV 1st user sign in 
+
+username = "1"
+password = "2"
+
+username1 = input("Give me your username").strip()
+password1 = input("Give me your password").strip()
+
+if username ==  username1 and  password == password1: 
+    print(f"Hello {username} welcomeback")
+else: 
+    print("your username is incorrect") 
+    
