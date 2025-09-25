a95be909fd0a1c602c508ee5f54bf259a126e7ff
MikaelaV15 <mikaela.vela@ucas-edu.net>
2025-09-25 10:28:10 -0600

    Update letter_grade.py
    added lines to code

--- letter_grade.py
+++ letter_grade.py
@@ -2,3 +2,11 @@
 
 grade = input("What is your grade: ")
 grade = input()
+
+if grade > 80:
+  print(f"You are doing good but a {grade} is only a B")
+elif grade == 100:
+  print(f"Wow {grade} is a perfect score you have an A+")
+else: 
+  print(f"Woah that {grade} is not a passing score")
+  
