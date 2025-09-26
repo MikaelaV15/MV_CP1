1d3daf5143ea9b2ae71e24323fc362f47cb86245
MikaelaV15 <mikaela.vela@ucas-edu.net>
2025-09-26 09:43:48 -0600

    practice finished

--- letter_grade.py
+++ letter_grade.py
@@ -1,9 +1,9 @@
 # MV 1st What is my grade 
 
-grade = input("What is your grade: ")
-grade = 90
+grade = int(input("What is your grade: "))
 
-if grade < 80:
+
+if grade > 80:
   print(f"You are doing good but a {grade} is only a B+")
 elif grade == 100:
   print(f"Wow {grade} is a perfect score you have an A+")
