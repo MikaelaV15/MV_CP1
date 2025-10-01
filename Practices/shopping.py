f57965a1f31279355b0e3fedae2f8aca8b5fd267
MikaelaV15 <mikaela.vela@ucas-edu.net>
2025-10-01 09:20:23 -0600

    practice finished

--- Practices/shopping.py
+++ Practices/shopping.py
@@ -1,8 +1,9 @@
 # MV 1st Shopping list manager 
 
 shopping = ["chocolate", "apples", "Bread", "takis"]
+start = True 
 
-while True:
+while start == True:
     action = int(input("Pick an option 1-4: If you pick 1 you will add an item from the list, if you pick 2 it will remove an item from your list, if you pick 3 you can view the list and 4 stops the list"))
     if action == 1:
         add = input("Pick what you would like to add to your list")
@@ -13,5 +14,10 @@ while True:
         item = input("What would you like to remove")
         shopping.remove(item) 
         print(f"{item} is now gone from your list")
-    if action == 3:
-        print("Your list consists of")
+    elif action == 3:
+        print("Your list consists of:")
+        print(shopping) 
+    else:
+        print("List says bye-bye")
+        start = False 
+        
---
+++ notes/for_loops.py
@@ -0,0 +1,3 @@
+# MV 1st For loops notes 
+
+nums = [4,654,]
