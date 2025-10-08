a783ce41404b9274d62b6b8406becbc19d016510
MikaelaV15 <mikaela.vela@ucas-edu.net>
2025-10-08 09:53:34 -0600

    practice created

--- Practices/rock_paper_scissors.py
+++ Practices/rock_paper_scissors.py
@@ -55,3 +55,4 @@ while win == False:
     start = int(input("Do you want to continue if so pick 1, if you want to end pick 2:"))
     if start == 2:
         win = True 
+        print("I see you're leaving.... bye")
