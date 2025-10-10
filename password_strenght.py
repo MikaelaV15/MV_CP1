bb5afb7561b56c4f2c4ebcd722fa9111d84d9322
MikaelaV15 <mikaela.vela@ucas-edu.net>
2025-10-10 09:53:17 -0600

    project started and pseudocode finished

--- password_strenght.py
+++ password_strenght.py
@@ -1,2 +1,36 @@
 # MV 1st Password strenght checker 
 
+#set the passwprd score to 0 
+#ask for password 
+
+#Take len() in the code and check that the length is over 8
+    #add +1 to score 
+
+#check for uppercase using isupper()
+    #check if true:
+    # if true +1 to score 
+   
+ 
+#check for lowercase using islower()
+    #check if true:
+    #if true +1 to score
+    
+
+#check for numbers using isdigit()
+    #check if true
+    #if true +1 to score 
+    
+
+#check for special chracters 
+    #if "!" in password or "@" in password or "#" in password or "$" in password or "%" in password or "^" in password or "&" in password or "*" in password or "(" in password or  ")" in password or "_" in password or "+" in password or "-" in password or "=" in password or "[" in password or "]" in password or "{" in password or "}" in password or "|" in password or ";" in password or ":" in password or "," in password or "." in password or "<" in password or ">" in password or "?"
+    #if true +1 to score 
+    
+# check for score 
+# if all true +5 
+#if four are true +4
+#if three are true +3 
+#if two are right +2
+#if one is right +1
+#if none right +0 
+
+
