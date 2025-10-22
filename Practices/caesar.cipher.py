14f6d65ccc5730640e38f99269f13eb4a9799cf1
MikaelaV15 <mikaela.vela@ucas-edu.net>
2025-10-22 09:52:36 -0600

    practice created and started

--- Practices/caesar.cipher.py
+++ Practices/caesar.cipher.py
@@ -1,3 +1,127 @@
 #MV 1st period Caesar Cipher 
 
+#Capital ASCII letters start at 65 and end at 90 
+#Lowercase letters start at 97 and end at 122
 
+#A is 65
+print(f"A = {ord("A")}")
+print(f"65 = {chr(98)}")
+#B is 66
+print(f"B = {ord("B")}")
+#C is 67
+
+#D is 68
+
+#E is 69 
+
+#F is 70 
+
+#G is 71
+
+#H is 72
+
+#I is 73
+
+#J is 74
+
+#K is 75
+
+#L is 76
+
+#M is 77
+
+#N is 78
+
+#O is 79
+
+#P is 80 
+
+#Q is 81
+
+#R os 82
+
+#S is 83
+
+#T is 84
+
+#U is 85
+
+#V is 86
+
+#W is 87
+
+#X is 88
+
+#Y is 89
+
+#Z is 90 
+
+
+
+#a is 97
+
+#b is 98
+
+#c is 99
+
+#d is 100
+
+#e if 101
+
+#f is 102
+
+#g is 103
+
+#h is 104 
+
+#i is 105
+
+#j is 106
+
+#k is 107
+
+#l is 108
+
+#m is 109
+
+#n is 110
+
+#o is 111
+
+#p is 112
+
+#q is 113
+
+#r is 114
+
+#s is 115
+
+#t is 116
+
+#u is 117
+
+#v is 118
+
+#w is 119
+
+#x is 120
+
+#y is 121
+
+#x is 122
+
+
+
+#Welcome to the Caesar Cipher for encode pick 1 for decode pick 2:
+
+#If the user picks 1:
+    #What message do you want to encode? 
+    #By how much would you like to shift?
+    #Show the message that was created
+#If the user picks 2: 
+    #What message would you like to decode?
+    #
+
+#your choice was  {  }. Plase enter the shift value: 
+
+#If they choose 1:
