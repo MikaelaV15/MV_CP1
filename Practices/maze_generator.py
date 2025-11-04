5c71ea10516a7585c4c13101625a1c0af70a171b
MikaelaV15 <mikaela.vela@ucas-edu.net>
2025-11-04 09:52:15 -0700

    practice continued and not finished

--- Practices/maze_generator.py
+++ Practices/maze_generator.py
@@ -1,12 +1,29 @@
 #MV 1st Maze generator 
 #import turtle and random 
-
+import turtle as t 
+import random as r
 #set the proportions (width and height of the maze)
-#make the coordinates of a 6 x 6 grid 
+#Set the height to 400 
+height = 400
+#set the width to 400 
+width = 400 
+#make the grid coordinates for a 6 by 6
+#make the row grid   
+row_grid = t.Turtle()
+column_grid = t.Turtle()
+row_grid = [[(0,0),(1,0),(2,0),(3,0),(4,0),(5,0)],
+           [(1,0),(1,1),(1,2),(1,3),(1,4),(1,5)],
+           [(2,0),(2,1),(2,2),(2,3),(2,4),(2,5)],
+           [(3,0),(3,1),(3,2),(3,3),(3,4),(3,5)],
+           [(4,0),(4,1),(4,2),(4,3),(4,4),(4,5)],
+           [(5,0),(5,1),(5,2),(5,3),(5,4),(5,5)]]
 
 
-
-#Make a funtion that creates lines in a column(col_line)
-    #col_line is a random position between 1 and 5 
-
+#make the colimn grid 
+column_grid = [[(0,0),(1,0),(2,0),(3,0),(4,0),(5,0)],
+           [(1,0),(1,1),(1,2),(1,3),(1,4),(1,5)],
+           [(2,0),(2,1),(2,2),(2,3),(2,4),(2,5)],
+           [(3,0),(3,1),(3,2),(3,3),(3,4),(3,5)],
+           [(4,0),(4,1),(4,2),(4,3),(4,4),(4,5)],
+           [(5,0),(5,1),(5,2),(5,3),(5,4),(5,5)]]
 
