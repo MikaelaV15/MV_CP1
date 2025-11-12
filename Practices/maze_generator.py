77b0c94917927f9a6107f4f7f8b2a36b9c172d4f
MikaelaV15 <mikaela.vela@ucas-edu.net>
2025-11-11 20:45:21 -0700

    Refactor maze generator logic and setup
    Refactor maze generator code for clarity and functionality.

--- Practices/maze_generator.py
+++ Practices/maze_generator.py
@@ -4,56 +4,45 @@ import turtle
 #import random for the walls 
 import random as r 
 
-#make variables for the grid some must have numbers 
 
+
+# set up the screen 
+screen = turtle.Screen() 
+screen.bgcolor("white")
+screen.title("Maze Generator")
+
+
+#make variables for the grid some must have numbers 
+#number of rows and colums 
 rows = 6 
 columns = 6 
+#how big each box is 
 cells_size = 30 
-row_grid = []
-col_grid = [] 
-walls = [] 
-visit = []
-
-back = "Maze Generator"
-pencolor = ("black")
-t = turtle.Pen()
-t.speed(0)
-t.hideturtle()
 
 #set up the turtle for the grid 
-def turtle_setup():
-    turtle.title("forgotten")
-    turtle.hideturtle
-    turtle.speed(5)
-    turtle.pensize(4)
-    turtle.penup
-
-#Set up the grid 
-def setup():
-    back = turtle.Screen()
-    back.bgcolor("black") 
-    back.title("Maze Generator") 
-    return back 
-    
-
-#Draw one of the walls 
-def walls():
-  turtle.penup() 
-  turtle.goto() 
-  turtle.pendown() 
-  turtle.begin_fill()
-  for i in range(4):
-        turtle.forward(cells_size) 
-        turtle.right(90) 
-turtle.end_fill()
+t = turtle.Turtle() 
+t.speed(0) 
+t.color("black") 
+t.pensize(5) 
+t.penup() 
+t.goto(-200,200) 
+#Set up the actual maze 
+#for loop inside of a for loop to make all the walls 
+for i in range(rows):
+    for j in range(colums):
+        #randomly make the walls 
+        if r.randint([True,False]):
+            #give it the area and the size of the space 
+            t.goto(-200 + j * cell_size, 200 + i * cell_size)
+            t.pendown() 
+            t.forward(cell_size)
+            t.penup() 
+            #make the turtle start again at the next row
+        t.goto(-200,200 * cell_size) 
 
 
 
-#Draw the grid 
-def grid_setup(boxes):
-    for i in range(rows*columns):
-        box = [1,1,1,1]
-        boxes.append(boxes) 
 
+t.hideturtle()
 
 turtle.done()
