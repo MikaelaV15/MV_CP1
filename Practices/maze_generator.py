5a36d6ed3094cbae885ea2a6c5ba63febc922624
MikaelaV15 <mikaela.vela@ucas-edu.net>
2025-11-12 09:54:08 -0700

    maze again

--- Practices/maze_generator.py
+++ Practices/maze_generator.py
@@ -3,9 +3,6 @@
 import turtle 
 #import random for the walls 
 import random as r 
-
-
-
 # set up the screen 
 screen = turtle.Screen() 
 screen.bgcolor("white")
@@ -14,7 +11,19 @@ screen.title("Maze Generator")
 
 #make variables for the grid some must have numbers 
 #number of rows and colums 
-rows = 6 
+grid_row = [[(0,0), (0,1),(0,2), (0,3), (0,4), (0,5)],
+        [(1,0), (1,1), (1,2), (1,3), (1,4), (1,5)],
+        [(2,0), (2,1), (2,2), (2,3), (2,4), (2,5)],
+        [(3,0), (3,1), (3,2), (3,3), (3,4), (3,5)],
+        [(4,0), (4,1), (4,2), (4,3), (4,4), (4,5)],
+        [(5,0), (5,1), (5,2), (5,3), (5,4), (5,5)]]
+rows = 6
+grid_col = [[(0,0), (0,1),(0,2), (0,3), (0,4), (0,5)]
+        [(1,0), (1,1), (1,2), (1,3), (1,4), (1,5)],
+        [(2,0), (2,1), (2,2), (2,3), (2,4), (2,5)],
+        [(3,0), (3,1), (3,2), (3,3), (3,4), (3,5)],
+        [(4,0), (4,1), (4,2), (4,3), (4,4), (4,5)],
+        [(5,0), (5,1), (5,2), (5,3), (5,4), (5,5)]]
 columns = 6 
 #how big each box is 
 cells_size = 30 
@@ -29,16 +38,21 @@ t.goto(-200,200)
 #Set up the actual maze 
 #for loop inside of a for loop to make all the walls 
 for i in range(rows):
-    for j in range(colums):
-        #randomly make the walls 
-        if r.randint([True,False]):
+    for j in range(columns):
+        #ask if they want to make it random
+        
             #give it the area and the size of the space 
-            t.goto(-200 + j * cell_size, 200 + i * cell_size)
+            t.goto(-200 + j * cells_size, 200 + i * cells_size)
             t.pendown() 
-            t.forward(cell_size)
+            t.forward(cells_size)
             t.penup() 
+        
+            t.goto(-200 + j * cells_size, 200 + i * cells_size)
+            t.penup()
+            t.forward(cells_size)
+            t.pendown()
             #make the turtle start again at the next row
-        t.goto(-200,200 * cell_size) 
+            t.goto(-200,200 - i  * cells_size) 
 
 
 
