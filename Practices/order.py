63c4f7e7625d3609a391cd335c52a1482382585e
MikaelaV15 <mikaela.vela@ucas-edu.net>
2025-11-18 09:54:47 -0700

    order up started

--- Practices/order.py
+++ Practices/order.py
@@ -1,6 +1,12 @@
 #MV 1st order up practice 
 #Make a dictionary list 
+menu_burgers = {
+    "bacon Mcdouble": "$3.19",
+    "Big Mac" : "$4.99",
+    "Double Quarter Pounder with cheese" : "$6.89",
+    "Quarter pounde with cheese bacon" : "$6.19",
 
+}
 
 #Add the menu and the prices 
 
