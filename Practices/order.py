858a45bb68ff18f2cd615c2a9cfbfa39a01be7ba
MikaelaV15 <mikaela.vela@ucas-edu.net>
2025-11-19 09:52:31 -0700

    order up and square roots started and continued

--- Practices/order.py
+++ Practices/order.py
@@ -1,16 +1,27 @@
 #MV 1st order up practice 
 #Make a dictionary list 
 menu_burgers = {
-    "bacon Mcdouble": "$3.19",
-    "Big Mac" : "$4.99",
-    "Double Quarter Pounder with cheese" : "$6.89",
-    "Quarter pounde with cheese bacon" : "$6.19",
-
+    "bacon Mcdouble": 3.19,
+    "Big Mac" : 4.99,
+    "Double Quarter Pounder with cheese" : 6.89,
+    "Quarter pounde with cheese bacon" : 6.19,
 }
 
-#Add the menu and the prices 
-
-
+menu_drinks = {
+    "Soda": 2.98,
+    "Slushie" : 3.87,
+    "Smoothie" : 4.69,
+    "Coffee" : 1.99
+}
+menu_sides = {
+    "Large Fries" : 3.99,
+    "Cheese Curds" : 4.78,
+    "Nacho Fries" : 5.89,
+    "Mozzarella sticks" : 7.43,
+    "Onion rings" : 2.89,
+    "Mashed potatoes" : 4.75,
+    "Mac and cheese" : 3.99
+}
 #Prompt the user tp select a drink and two side dishes 
 
 
---
+++ Practices/square_roots.py
@@ -0,0 +1,8 @@
+#MV 1st Square roots 
+#Make the list of a minimum of 20 numbers 
+numbers = [3, 7, 12, 25, 30, 45, 50, 65, 70, 85, 90, 105, 110, 125, 130, 145, 150, 165, 170, 185, 190, 205, 210, 225, 230, 245, 250, 265, 270, 285]
+#Make a function 
+new_numbers = map(lambda num:num**2, numbers)
+#make a loop 
+for number in new_numbers: 
+    print(f"print{numbers} and {new_numbers}")
