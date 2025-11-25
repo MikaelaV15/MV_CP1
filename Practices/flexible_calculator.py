6191871eeb9990f57e2972471d6b9152dd4cb394
MikaelaV15 <mikaela.vela@ucas-edu.net>
2025-11-25 09:51:35 -0700

    practices finished

---
+++ Practices/flexible_calculator.py
@@ -0,0 +1,20 @@
+#MV 1st Flexible Calculator 
+#Introduce the user to the calculator 
+
+#Ask the user what operations they would like  to use 
+    #print what operation 
+#ask the user to enter number 
+    #Tell them to type done when no more numbers needed 
+#Return the numbers 
+    #tell them what operation you are performing 
+#Give the final calculation 
+
+#Ask the user if they would like to do it again 
+#start = int(input("would you like to play again, if yes press 1 if not press 2 "))
+#if start == 2:
+    #start = False
+    #print("bye traitorrrr")
+#if start == 1:
+    #Start = True 
+    #print("Welcome BACKK")
+
