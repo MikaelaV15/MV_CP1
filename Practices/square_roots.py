4aa338338628b7f4156736724e5774ce10a8d5d8
MikaelaV15 <mikaela.vela@ucas-edu.net>
2025-11-25 09:23:44 -0700

    squared roots finished

--- Practices/square_roots.py
+++ Practices/square_roots.py
@@ -3,6 +3,8 @@
 numbers = [3, 7, 12, 25, 30, 45, 50, 65, 70, 85, 90, 105, 110, 125, 130, 145, 150, 165, 170, 185, 190, 205, 210, 225, 230, 245, 250, 265, 270, 285]
 #Make a function 
 new_numbers = map(lambda num:num**2, numbers)
+i = 0 
 #make a loop 
-for numbers in new_numbers: 
-    
+for nums in new_numbers: 
+    print(f"Original {numbers[i]}, Squared Root {nums}")
+    i += 1
